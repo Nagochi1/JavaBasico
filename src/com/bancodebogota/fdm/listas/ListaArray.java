b49f8562b2a6b83aa569f190b357ba0f760586bd
Nagochi1 <nagochi@hotmail.com>
2018-07-11 21:29:37 -0500

    Actualizacion del ejercicio

---
+++ src/com/bancodebogota/fdm/listas/ListaArray.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.bancodebogota.fdm.listas;
+
+import java.util.Arrays;
+
+/**
+ *
+ * @author NAGOCHI
+ */
+public class ListaArray {
+    
+    private Object[] dato;
+    private int dimension;
+    private int incremento;
+    private int posicion;
+
+    public ListaArray() {
+        this.dato = new Object[this.dimension];
+        this.posicion=0;
+    }
+    
+    
+    
+    
+    
+    
+}
