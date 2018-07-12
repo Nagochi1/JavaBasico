68e4cd99c0b517c0f54fa8d95df69766ba0e8dbd
Nagochi1 <nagochi@hotmail.com>
2018-07-11 21:04:21 -0500

    Actualizacion del ejercicio

--- src/com/bancodebogota/fdm/listas/JavaBasico.java
+++ src/com/bancodebogota/fdm/listas/JavaBasico.java
@@ -18,16 +18,16 @@ public class JavaBasico {
 
         ListaSimple lista = new ListaSimple();
 
-        lista.ingresarNodo(1);
-        lista.ingresarNodo(2);
-        lista.ingresarNodo(3);
-        lista.ingresarNodo(4);
-        lista.ingresarNodo(5);
-        lista.ingresarNodo(6);
-        lista.ingresarNodo(7);
-        lista.ingresarNodo(8);
-        lista.ingresarNodo(9);
-        lista.ingresarNodo(10);
+        lista.ingresarNodo("1");
+        lista.ingresarNodo("2");
+        lista.ingresarNodo("3");
+        lista.ingresarNodo("4");
+        lista.ingresarNodo("5");
+        lista.ingresarNodo("6");
+        lista.ingresarNodo("7");
+        lista.ingresarNodo("8");
+        lista.ingresarNodo("9");
+        lista.ingresarNodo("10");
 
         lista.imprimirLista();
 
