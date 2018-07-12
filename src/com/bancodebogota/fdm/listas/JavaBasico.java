501e0fda34e66b6d5ce7a401af8da4c43741624f
Nagochi1 <nagochi@hotmail.com>
2018-07-11 22:09:37 -0500

    Actualizacion del ejercicio

--- src/com/bancodebogota/fdm/listas/JavaBasico.java
+++ src/com/bancodebogota/fdm/listas/JavaBasico.java
@@ -15,9 +15,9 @@ public class JavaBasico {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-
+        
         ListaSimple lista = new ListaSimple();
-
+        
         lista.ingresarNodo("1");
         lista.ingresarNodo("2");
         lista.ingresarNodo("3");
@@ -28,8 +28,25 @@ public class JavaBasico {
         lista.ingresarNodo("8");
         lista.ingresarNodo("9");
         lista.ingresarNodo("10");
-
+        
+        System.out.println("Lista Simple");
         lista.imprimirLista();
+        
+        ListaArray listaArray = new ListaArray();
+        
+        listaArray.insertar("1");
+        listaArray.insertar("2");
+        listaArray.insertar("3");
+        listaArray.insertar("4");
+        listaArray.insertar("5");
+        listaArray.insertar("6");
+        listaArray.insertar("7");
+        listaArray.insertar("8");
+        listaArray.insertar("9");
+        listaArray.insertar("10");
+        
+        System.out.println("\nLista Array");
+        listaArray.imprimir();
 
     }
 
